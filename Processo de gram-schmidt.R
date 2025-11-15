5bbef4e8881805f9428e157e62cfdf6f57111e30
Kuroikenshiga <juniorsilvapalafoz@gmail.com>
2025-11-15 09:04:56 -0300

    Adição de uma cor mais forte ao gráfico

--- Processo de gram-schmidt.R
+++ Processo de gram-schmidt.R
@@ -1,4 +1,3 @@
-
 GramSchmidt <- function(espaco,ortonormalizar){
   if(!is.list(espaco) || length(espaco) < 1)return(NULL);
   espaco_ortogonal <- list()
@@ -20,16 +19,42 @@ GramSchmidt <- function(espaco,ortonormalizar){
   return(espaco_ortogonal)
 }
 
-#espaco_vetorial <- list(
- # c(1,1,0),
-#  c(1,0,1),
-#  c(0,1,1)
-#)
-
-#cat(str(GramSchmidt(espaco_vetorial,FALSE)))
-
 espaco_vetorial <- list(
-  c(2,0),
-  c(1,1)
+  c(1,1),
+  c(1,2)
+  
+  
 )
-cat(str(GramSchmidt(espaco_vetorial,TRUE)))
+
+plot(0, 0, xlim = c(-2, 2), ylim = c(-1, 3), type = "n",
+     xlab = "Eixo X", ylab = "Eixo Y",main = "Base original", asp = 1)
+
+abline(h=0, v=0, col="black")
+
+arrows(0, 0, espaco_vetorial[[1]][1], espaco_vetorial[[1]][2], col="red", lwd=2, angle=15)
+arrows(0, 0, espaco_vetorial[[2]][1], espaco_vetorial[[2]][2], col="blue", lwd=2, angle=15)
+cat(str(espaco_vetorial,TRUE))
+
+espaco_ortogonalizado <- GramSchmidt(espaco_vetorial,FALSE)
+plot(0, 0, xlim = c(-2, 2), ylim = c(-1, 3), type = "n",
+     xlab = "Eixo X", ylab = "Eixo Y",main = "Base ortogonalizada", asp = 1)
+
+abline(h=0, v=0, col="black")
+
+arrows(0, 0, espaco_ortogonalizado[[1]][1], espaco_ortogonalizado[[1]][2], col="red", lwd=2, angle=15)
+arrows(0, 0, espaco_ortogonalizado[[2]][1], espaco_ortogonalizado[[2]][2], col="blue", lwd=2, angle=15)
+cat(str(espaco_ortogonalizado,TRUE))
+
+espaco_ortonormalizado <- GramSchmidt(espaco_vetorial,TRUE)
+plot(0, 0, xlim = c(-2, 2), ylim = c(-1, 3), type = "n",
+     xlab = "Eixo X", ylab = "Eixo Y",main = "Base ortonormalizada", asp = 1)
+
+abline(h=0, v=0, col="black")
+
+arrows(0, 0, espaco_ortonormalizado[[1]][1], espaco_ortonormalizado[[1]][2], col="red", lwd=2, angle=15)
+arrows(0, 0, espaco_ortonormalizado[[2]][1], espaco_ortonormalizado[[2]][2], col="blue", lwd=2, angle=15)
+cat(str(espaco_ortonormalizado,TRUE))
+
+
+
+
